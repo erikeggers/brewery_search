64a2f79ac569cc8be99ead6c18e92be2446b1e9e
Erik Eggers <em.eggers@gmail.com>
2015-02-11 00:27:22 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,30 @@
 var AppRouter = Backbone.Router.extend({
     routes: {
       "": "index",
-      // "results/:id": "results",
-      // "brewery/:id": "brewery"
+      "results/:id": "results",
+      "brewery/:id": "brewery"
     },
 
   index: function(){
-    var template = _.template( $('#brewery-search-template').text() );
-    var renderedTemplate = template;
+    var template = _.template(  $('#brewery-search-template').text() );
+    var objectForTemplate = { 'index':'myVariable' };
+    var renderedTemplate = template(objectForTemplate);
     $('.app-container').html(renderedTemplate);
   },
 
-  // results: function(){
-  //   var template = _.template( $('#brewery-list-template').text() );
-  //   var renderedTemplate = template;
-  //   $('.app-container').html(renderedTemplate);
-  // },
-  //
-  // brewery: function(){
-  //   var template = _.template( $('#brewery-info-template').text() );
-  //   var renderedTemplate = template;
-  //   $('.app-container').html(renderedTemplate);
-  // }
+  results: function(id){
+    var template = _.template( $('#brewery-list-template').text() );
+    var objectForTemplate = { 'index' : 'someRandomVariable', 'urlReference' : id };
+    var renderedTemplate = template(objectForTemplate);
+    $('.app-container').html(renderedTemplate);
+  },
+
+  brewery: function(id){
+    var template = _.template( $('#brewery-info-template').text() );
+    var objectForTemplate = { 'index' : 'someRandomVariableAgain', 'urlReference' : id };
+    var renderedTemplate = template(objectForTemplate);
+    $('.app-container').html(renderedTemplate);
+  }
 
 
 });

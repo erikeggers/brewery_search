31a3cbe2da68a5a1b01214f7f74d32db5a982a72
Erik Eggers <em.eggers@gmail.com>
2015-02-13 19:41:09 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,122 @@
-var AppRouter = Backbone.Router.extend({
-    routes: {
-      "": "index",
-      "results/:id": "results",
-      "brewery/:id": "brewery"
+(function(){
+
+  var searchTerm;
+
+  window.App = window.App || {};
+
+  var AppModel = Backbone.Model.extend({
+    defaults: {
+      searchTerm: ''
+    }
+  });
+
+  var State = Backbone.Model.extend({
+    defaults: {
+      body: ''
+    }
+  });
+
+  var Result = Backbone.Model.extend({});
+
+  var ResultsCollection = Backbone.Collection.extend({
+    initialize: function(collection, options){
+      this.appModel = options.appModel;
     },
 
-  index: function(){
-    var template = _.template(  $('#brewery-search-template').text() );
-    var objectForTemplate = { 'index':'myVariable' };
-    var renderedTemplate = template(objectForTemplate);
-    $('.app-container').html(renderedTemplate);
+    url: function (){
+      var cors = 'https://jsonp.nodejitsu.com/?url=';
+      var searchTerm = this.appModel.get('searchTerm').replace(/ /g, '+');
+      var base = "http://api.brewerydb.com/v2/locations?region=";
+      var keyBase = "&key=";
+      var key = "da7893f044ebeecf79010b9e07bc84aa";
+      return cors + encodeURIComponent(base + searchTerm + keyBase + key);
+    },
+
+    model: Result,
+
+    parse: function(response) {
+     return response.data;
+   }
+});
+
+var TextInputView = Backbone.View.extend({
+  el: 'form',
+  events: {
+    'submit': 'stateSearch'
   },
 
-  results: function(id){
-    var template = _.template( $('#brewery-list-template').text() );
-    var objectForTemplate = { 'index' : 'someRandomVariable', 'urlReference' : id };
-    var renderedTemplate = template(objectForTemplate);
-    $('.app-container').html(renderedTemplate);
-  },
-
-  brewery: function(id){
-    var template = _.template( $('#brewery-info-template').text() );
-    var objectForTemplate = { 'index' : 'someRandomVariableAgain', 'urlReference' : id };
-    var renderedTemplate = template(objectForTemplate);
-    $('.app-container').html(renderedTemplate);
+  stateSearch: function(event) {
+    event.preventDefault();
+    searchTerm = this.$('input').val();
+    this.$('input').val('');
+    window.location = '#results/' + searchTerm.replace(/ /g, '+');
   }
 
-
 });
 
-$(document).ready(function(){
-  var router = new AppRouter();
-  Backbone.history.start();
-});
+var ResultsView = Backbone.View.extend({
+
+   el: '#results',
+
+   initialize: function(){
+     this.listenTo(this.collection, 'sync', this.render);
+   },
+
+   render: function(){
+     this.$el.empty();
+     var self = this;
+     this.collection.each(function(result){
+       self.$el.append('<li><a href="#results/' + searchTerm.replace(/ /g, '+') + "/" + result.get('breweryId') + '">' + result.get('brewery').name + '</a></li>');
+
+     });
+   }
+ });
+
+ var BreweryView = Backbone.View.extend({
+
+  //  template: _.template( $('#brewery-info-template').text() ),
+
+
+ });
+
+ var AppRouter = Backbone.Router.extend({
+   routes: {
+     '': 'index',
+     'results/:state': 'search',
+     'results/:state/:brewery': 'details'
+
+   },
+
+   initialize: function(){
+     this.appModel = new AppModel();
+     this.list = new ResultsCollection([], {appModel: this.appModel});
+     this.listView = new ResultsView({collection: this.list});
+    //  this.listView.render();
+   },
+
+   index: function(){
+    //  this.list.fetch();
+   },
+
+   search: function(state){
+     this.appModel.set('searchTerm', state);
+     this.list.fetch();
+     this.listView.render();
+   },
+
+   details: function(state, brewery){
+     this.appModel.set('searchTerm', state);
+     this.list.fetch();
+     console.log(this.list.findWhere({breweryId: brewery.toString()}));
+    //  this.listView.render();
+   }
+ });
+
+ window.inputview = new TextInputView({model: State});
+
+
+ $(document).ready(function(){
+   window.router = new AppRouter();
+   Backbone.history.start();
+ });
+})();

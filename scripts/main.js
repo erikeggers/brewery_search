2e863d91fca83a114d37d0a41ef00d9beefe57de
Erik Eggers <em.eggers@gmail.com>
2015-02-15 13:14:21 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
-(function(){
+/*globals Backbone, _  */
+
+(function() {
+
+  'use strict';
 
   var searchTerm;
 
@@ -19,109 +23,138 @@
   var Result = Backbone.Model.extend({});
 
   var ResultsCollection = Backbone.Collection.extend({
-    initialize: function(collection, options){
+    initialize: function(collection, options) {
       this.appModel = options.appModel;
     },
 
-    url: function (){
+    url: function() {
       var cors = 'https://jsonp.nodejitsu.com/?url=';
       var searchTerm = this.appModel.get('searchTerm').replace(/ /g, '+');
-      var base = "http://api.brewerydb.com/v2/locations?region=";
-      var keyBase = "&key=";
-      var key = "da7893f044ebeecf79010b9e07bc84aa";
+      var base = 'http://api.brewerydb.com/v2/locations?region=';
+      var keyBase = '&key=';
+      // var key = 'da7893f044ebeecf79010b9e07bc84aa';
+      var key = 'ebc1178ffae225ad44a88dcb1d8e221c';
       return cors + encodeURIComponent(base + searchTerm + keyBase + key);
     },
 
     model: Result,
 
     parse: function(response) {
-     return response.data;
-   }
-});
+      return response.data;
+    }
+  });
 
-var TextInputView = Backbone.View.extend({
-  el: 'form',
-  events: {
-    'submit': 'stateSearch'
-  },
+  var TextInputView = Backbone.View.extend({
+    el: 'form',
+    events: {
+      'submit': 'stateSearch'
+    },
 
-  stateSearch: function(event) {
-    event.preventDefault();
-    searchTerm = this.$('input').val();
-    this.$('input').val('');
-    window.location = '#results/' + searchTerm.replace(/ /g, '+');
-  }
+    stateSearch: function(event) {
+      event.preventDefault();
+      searchTerm = this.$('input').val();
+      this.$('input').val('');
+      window.location = '#results/' + searchTerm.replace(/ /g, '+');
+    }
 
-});
+  });
 
-var ResultsView = Backbone.View.extend({
+  var ResultsView = Backbone.View.extend({
 
-   el: '#results',
+    el: '#results',
 
-   initialize: function(){
-     this.listenTo(this.collection, 'sync', this.render);
-   },
+    events: {
+      'click': 'renderBrewery'
+    },
 
-   render: function(){
-     this.$el.empty();
-     var self = this;
-     this.collection.each(function(result){
-       self.$el.append('<li><a href="#results/' + searchTerm.replace(/ /g, '+') + "/" + result.get('breweryId') + '">' + result.get('brewery').name + '</a></li>');
+    initialize: function() {
+      this.listenTo(this.collection, 'sync', this.render);
+    },
 
-     });
-   }
- });
+    render: function() {
+      this.$el.empty();
+      var self = this;
+      this.collection.each(function(result) {
+        self.$el.append('<li><a href="#results/' + searchTerm.replace(/ /g, '+') + '/' + result.get('breweryId') + '">' + result.get('brewery').name + '</a></li>');
 
- var BreweryView = Backbone.View.extend({
+      });
+    },
+  });
 
-   el: 'a',
-   events: {
-  'click': 'template'
-  },
+  //   renderBrewery: function() {
+  //     var breweryView = new BreweryView();
+  //   }
+  // });
 
-  template: _.template( $('#brewery-info-template').text() ),
+  var BreweryView = Backbone.View.extend({
+     el: '#details',
 
+     initialize: function(){
+       this.listenTo(this.collection, 'sync', this.render);
+     },
 
- });
+     render: function() {
+       this.$('input');
+       this.$el.empty();
 
- var AppRouter = Backbone.Router.extend({
-   routes: {
-     '': 'index',
-     'results/:state': 'search',
-     'results/:state/:brewery': 'details'
+       var test = this.collection.find(function(results){
+         return results.get('breweryId') === breweryId;
+       });
 
-   },
+       console.log(this.collection);
 
-   initialize: function(){
-     this.appModel = new AppModel();
-     this.list = new ResultsCollection([], {appModel: this.appModel});
-     this.listView = new ResultsView({collection: this.list});
-    //  this.listView.render();
-   },
+       console.log(test);
 
-   index: function(){
-    //  this.list.fetch();
-   },
+       console.log(test.attributes.brewery.name);
 
-   search: function(state){
-     this.appModel.set('searchTerm', state);
-     this.list.fetch();
-     this.listView.render();
-   },
-
-   details: function(state, brewery){
-     this.appModel.set('searchTerm', state);
-     this.list.fetch();
-     console.log(this.list.findWhere({breweryId: brewery.toString()}));
-    //  this.listView.render();
-   }
- });
-
- window.inputview = new TextInputView({model: State});
+       this.$el.append('this is where the brewery info goes');
+     }
 
 
- $(document).ready(function(){
-   window.router = new AppRouter();
-   Backbone.history.start();
- });
+   });
+
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+      '': 'index',
+      'results/:state': 'search',
+      'results/:state/:brewery': 'details'
+
+    },
+
+    initialize: function() {
+      this.appModel = new AppModel();
+      this.list = new ResultsCollection([], {
+        appModel: this.appModel
+      });
+      this.listView = new ResultsView({
+        collection: this.list
+      });
+    },
+
+    index: function() {},
+
+    search: function(state) {
+      this.appModel.set('searchTerm', state);
+      this.list.fetch();
+      this.listView.render();
+    },
+
+    details: function(state, brewery) {
+      this.appModel.set('searchTerm', state);
+      this.list.fetch();
+
+      var myBreweryView = new BreweryView;
+      breweryView.render();
+      console.log('here');
+    }
+  });
+
+  window.inputview = new TextInputView({
+    model: State
+  });
+
+  $(document).ready(function() {
+    window.router = new AppRouter();
+    Backbone.history.start();
+  });
 })();
